90e520344474299aa57bc899e908dd82d356c997
David Daniels <dldaniels07@yahoo.com>
2020-10-23 22:23:38 -0600

    Dinosaurs
    git status
    
    :q
    :qw
    :wq

---
+++ DinosaurCrud/src/main/java/com/skilldistillery/jpacrud/DinosaurCrudApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinosaurCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinosaurCrudApplication.class, args);
+	}
+
+}
