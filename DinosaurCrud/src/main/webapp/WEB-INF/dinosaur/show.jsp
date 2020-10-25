82d46d8666e540b78d81056a2c54f6782cfb0b4b
David Daniels <dldaniels07@yahoo.com>
2020-10-24 23:20:58 -0600

    show.jsp

--- DinosaurCrud/src/main/webapp/WEB-INF/dinosaur/show.jsp
+++ DinosaurCrud/src/main/webapp/WEB-INF/dinosaur/show.jsp
@@ -4,13 +4,57 @@
 <html>
 <head>
 <meta charset="UTF-8">
+<link rel="stylesheet"
+	href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+	integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+	crossorigin="anonymous">
 <title>Show Dinosaur</title>
 </head>
 <body>
+<div class="container fluid">
 
 <div>
-<h3>${dinosaur.name}</h3>
+<h2>${dinosaur.name}</h2>
+<h4>"${dinosaur.nameMeaning}"</h4>
+<h4><em>${dinosaur.pronunciation}</em></h4>
+<table class="table table-striped">
+		<thead class="thead thead-dark">
+		  <tr>
+			<th>Type</th>
+			<th>Diet</th>
+			<th>Location</th>
+			<th>Period</th>
+		  </tr>
+		</thead>
+		<tbody>
+			<tr>
+					<td>${dinosaur.type}</td>
+					<td>${dinosaur.diet}</td>
+					<td>${dinosaur.areaLived}</td>
+					<td>${dinosaur.timePeriodLived}</td>
+				</tr>
+			
+		</tbody>
+	</table>
+<img src="${dinosaur.image}" alt=""width="400" height="300">
+<h3>${dinosaur.description}</h3>
+<%-- <h3>${dinosaur.diet} ${dinosaur.type}</h3>
+<br> --%>
+<h3>${dinosaur.weightKg} ${dinosaur.heightM} ${dinosaur.lengthM} </h3>
+<br>
+<h3>${dinosaur.taxonomy}</h3>
 </div>
-
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+		integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+		crossorigin="anonymous"></script>
+	<script
+		src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+		integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN"
+		crossorigin="anonymous"></script>
+	<script
+		src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+		integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s"
+		crossorigin="anonymous"></script>
+</div>
 </body>
 </html>
--- JPADinosaur/src/main/java/com/skilldistillery/jpacrud/entities/Dinosaur.java
+++ JPADinosaur/src/main/java/com/skilldistillery/jpacrud/entities/Dinosaur.java
@@ -1,5 +1,6 @@
 package com.skilldistillery.jpacrud.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,12 +8,45 @@ import javax.persistence.Id;
 
 @Entity
 public class Dinosaur {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
 	private String name;
+	
+	private String diet;
+	
+    @Column(name="area_lived")
+	private String areaLived;
+    
+    @Column(name="time_period_lived")
+	private String timePeriodLived;
+
+	private String description;
+
+	@Column(name="weight_in_kg")
+	private Integer weightKg;
+	
+	@Column(name="height_in_meters")
+	private Integer heightM;
+	
+	@Column(name="length_in_meters")
+	private Integer lengthM;
+	
+	@Column(name="type_of_dinosaur")
+	private String type;
+	
+	@Column(name="name_pronunciation")
+	private String pronunciation;
+	
+	@Column(name="name_meaning")
+	private String nameMeaning;
+
+	private String taxonomy;
+
+	
+	private String image;
 
 	public Dinosaur() {
 		super();
@@ -34,11 +68,114 @@ public class Dinosaur {
 		this.name = name;
 	}
 
+	public String getDiet() {
+		return diet;
+	}
+
+	public void setDiet(String diet) {
+		this.diet = diet;
+	}
+
+	public String getAreaLived() {
+		return areaLived;
+	}
+
+	public void setAreaLived(String areaLived) {
+		this.areaLived = areaLived;
+	}
+
+	public String getTimePeriodLived() {
+		return timePeriodLived;
+	}
+
+	public void setTimePeriodLived(String timePeriodLived) {
+		this.timePeriodLived = timePeriodLived;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getWeightKg() {
+		return weightKg;
+	}
+
+	public void setWeightKg(Integer weightKg) {
+		this.weightKg = weightKg;
+	}
+
+	public Integer getHeightM() {
+		return heightM;
+	}
+
+	public void setHeightM(Integer heightM) {
+		this.heightM = heightM;
+	}
+
+	public Integer getLengthM() {
+		return lengthM;
+	}
+
+	public void setLengthM(Integer lengthM) {
+		this.lengthM = lengthM;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getPronunciation() {
+		return pronunciation;
+	}
+
+	public void setPronunciation(String pronunciation) {
+		this.pronunciation = pronunciation;
+	}
+
+	public String getNameMeaning() {
+		return nameMeaning;
+	}
+
+	public void setNameMeaning(String nameMeaning) {
+		this.nameMeaning = nameMeaning;
+	}
+
+	public String getTaxonomy() {
+		return taxonomy;
+	}
+
+	public void setTaxonomy(String taxonomy) {
+		this.taxonomy = taxonomy;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		builder.append("Dinosaur [id=").append(id).append(", name=").append(name).append("]");
+		builder.append("Dinosaur [id=").append(id).append(", name=").append(name).append(", diet=").append(diet)
+				.append(", areaLived=").append(areaLived).append(", timePeriodLived=").append(timePeriodLived)
+				.append(", description=").append(description).append(", weightKg=").append(weightKg)
+				.append(", heightM=").append(heightM).append(", lengthM=").append(lengthM).append(", type=")
+				.append(type).append(", pronunciation=").append(pronunciation).append(", nameMeaning=")
+				.append(nameMeaning).append(", taxonomy=").append(taxonomy).append("]");
 		return builder.toString();
 	}
 
 }
+
+
